cb9447b56d2bc18b696c999b2502c4f9ced9e4c6
Minh Quy <onequy@gmail.com>
2024-04-22 11:45:12 +0700

    app

---
+++ src/App/Screens/SetupScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SetupScreen = () => {
+  return (
+    <View>
+      <Text>SetupScreen</Text>
+    </View>
+  )
+}
+
+export default SetupScreen
