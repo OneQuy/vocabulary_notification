81a569a6a3a4bdda45164f53d9a5e5e1c2bb46b3
Minh Quy <onequy@gmail.com>
2024-04-23 08:09:52 +0700

    theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, SafeAreaView, StyleSheet } from 'react-native'
+import React, { useMemo } from 'react'
 import SetupScreen from './src/App/Screens/SetupScreen'
+import useTheme from './src/App/Hooks/useTheme'
 
 const App = () => {
+  const theme = useTheme()
+
+  const style = useMemo(() => {
+    return StyleSheet.create({
+      master: { flex: 1, backgroundColor: theme.background }
+    })
+  }, [theme])
+
   return (
-    <SafeAreaView>
-      <SetupScreen />
+    <SafeAreaView style={style.master}>
+      <View style={style.master}>
+        <SetupScreen />
+      </View>
     </SafeAreaView>
   )
 }
--- src/App/Screens/SetupScreen.tsx
+++ src/App/Screens/SetupScreen.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useMemo } from 'react'
+import { CommonStyles } from '../../Common/CommonConstants'
+import { FontSize } from '../Constants/Constants_FontSize'
+import useTheme from '../Hooks/useTheme'
 
 const SetupScreen = () => {
+  const theme = useTheme()
+
+  const style = useMemo(() => {
+    return StyleSheet.create({
+      title: { fontSize: FontSize.Normal, color: theme.primary}
+    })
+  }, [theme])
+
   return (
-    <View>
-      <Text>SetupScreen</Text>
+    <View style={CommonStyles.flex_1}>
+      <Text style={style.title}>SetupScreen</Text>
     </View>
   )
 }
