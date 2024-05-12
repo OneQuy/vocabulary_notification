97f123d77246482202091e935c7ac8ad5e16bcd9
Minh Quy <onequy@gmail.com>
2024-05-12 20:36:31 +0700

    theme

--- src/App/Hooks/useTheme.ts
+++ src/App/Hooks/useTheme.ts
@@ -1,21 +1,3 @@
-const Dark_Default = {
-    background: '#1c1c1c',
-    counterBackground: '#ffffff',
-    
-    primary: '#dda',
-    counterPrimary: '#1c1c1c',
-
-    isDarkTheme: true,
-} as const
-
-type Theme = typeof Dark_Default
-
-const useTheme = () : Theme => {
-  return Dark_Default
-}
-
-export default useTheme
-
 export const Color_BG = '#151517'
 
 export const Color_BG2 = '#1a1a1a'
