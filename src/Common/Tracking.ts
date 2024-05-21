3f9a27a9cfc54b6cf54dbd80913ce800b20ebb9b
Minh Quy <onequy@gmail.com>
2024-05-21 08:07:40 +0700

    TrackSimple

--- src/Common/Tracking.ts
+++ src/Common/Tracking.ts
@@ -18,7 +18,7 @@
 //          * __DEV__ === false => dev mode            
 
 import { Alert } from "react-native"
-import Aptabase, { trackEvent } from "@aptabase/react-native";
+import Aptabase, { trackEvent as AptabaseTrack } from "@aptabase/react-native";
 import { IsDev } from "./IsDev";
 import { GetRemoteConfigWithCheckFetchAsync } from "./RemoteConfig";
 import { ApatabaseKey_Dev, ApatabaseKey_Production } from "../../Keys"
@@ -80,143 +80,6 @@ const GetFinalAptabaseIgnoredEventNamesAsync = async (): Promise<string[]> => {
 }
 
 /**
- * must be called after
- * 
- *      + GetRemoteConfigWithCheckFetchAsync()
- *      + IsDev()
- */
-export const InitTrackingAsync = async (instancePosthog: PostHog) => {
-    if (inited)
-        return
-
-    inited = true
-
-    if (IsLog)
-        console.log('[InitTrackingAsync] initting...');
-
-    // post hog
-
-    posthog = instancePosthog
-
-    // aptabase
-
-    const appConfig = await GetRemoteConfigWithCheckFetchAsync()
-
-    const productionKey = SafeValue(appConfig?.tracking?.aptabaseProductionKey, ApatabaseKey_Production)
-
-    Aptabase.init(
-        (IsDev() && !__DEV__) ? // mobile (release) dev
-            ApatabaseKey_Dev : // dev
-            productionKey // prodution
-    )
-}
-
-export const TrackingAsync = async (
-    eventName: string,
-    firebasePaths: string[],
-
-    /**
-     * aptabase, posthog,... (not firebase)
-     */
-    trackingValuesObject?: Record<string, string | number | boolean>
-): Promise<void> => {
-    if (!inited) {
-        console.error('[TrackingAsync] not initted yet.');
-        return
-    }
-
-    const appConfig = await GetRemoteConfigWithCheckFetchAsync()
-
-    if (IsDev())
-        eventName = 'dev__' + eventName
-
-    if (IsLog)
-        console.log('------------------------')
-
-
-    // track aptabase
-
-    const finalAptabaseIgnoredEventNames = await GetFinalAptabaseIgnoredEventNamesAsync()
-
-    const shouldTrackAptabase =
-        (!appConfig || !appConfig.tracking || appConfig.tracking.enableAptabase !== false) &&
-        (!finalAptabaseIgnoredEventNames.includes(eventName))
-
-    console.log(shouldTrackAptabase, inited, finalAptabaseIgnoredEventNames);
-
-    if (shouldTrackAptabase) {
-        trackEvent(eventName, trackingValuesObject)
-
-        if (IsLog) {
-            console.log('tracking [APTABASE]: ', eventName, JSON.stringify(trackingValuesObject));
-        }
-    }
-
-
-    // track firebase
-
-    const shouldTrackFirebase = !appConfig || !appConfig.tracking || appConfig.tracking.enableFirebase !== false
-
-    if (shouldTrackFirebase) {
-        for (let i = 0; i < firebasePaths.length; i++) {
-            let path = GetPrefixFbTrackPath() + firebasePaths[i]
-            path = path.replaceAll('#d', GetTodayStringUnderscore())
-
-            if (IsLog) {
-                console.log('tracking [FIREBASE]: ', path);
-            }
-
-            FirebaseDatabase_IncreaseNumberAsync(path, 0)
-        }
-    }
-
-
-    // track posthog
-
-    const shouldTrackPosthog =
-        !appConfig ||
-        !appConfig.tracking ||
-        appConfig.tracking.enablePosthog !== false
-
-    if (shouldTrackPosthog && posthog) {
-        posthog.capture(eventName, trackingValuesObject)
-
-        if (IsLog) {
-            console.log('tracking [POSTHOG]: ', eventName, JSON.stringify(trackingValuesObject))
-        }
-    }
-
-    if (IsLog)
-        console.log('****************')
-}
-
-/**
- * CAN USE ANYWHERE
- * 
- * Usage: HandleError(resOrError, 'DataToNotification', false)
- */
-export const HandleError = (error: any, root: string, alert = true) => {
-    const sError = SafeValue(error?.message, '' + ToCanPrint(error))
-
-    // tracking firebase
-
-    CheckAndTrackErrorOnFirebaseAsync(sError, root)
-
-    // alert
-
-    if (alert) {
-        Alert.alert(
-            'Error',
-            sError
-        )
-    }
-    else if (__DEV__) {
-        const content = `[${root}] ${sError}`
-        console.error(content);
-    }
-}
-
-/**
  * 
  * @returns undefined will not track
  */
@@ -265,4 +128,149 @@ const CheckAndTrackErrorOnFirebaseAsync = async (error: string, root: string, su
 
     const path = `${FirebaseTrackingProductionPath}errors/${root}/${subpath ? (subpath + '/') : ''}${Date.now()}`
     FirebaseDatabase_SetValueAsync(path, error)
+}
+
+/**
+ * must be called after
+ * 
+ *      + GetRemoteConfigWithCheckFetchAsync()
+ *      + IsDev()
+ */
+export const InitTrackingAsync = async (instancePosthog: PostHog) => {
+    if (inited)
+        return
+
+    inited = true
+
+    if (IsLog)
+        console.log('[InitTrackingAsync] initting...');
+
+    // post hog
+
+    posthog = instancePosthog
+
+    // aptabase
+
+    const appConfig = await GetRemoteConfigWithCheckFetchAsync()
+
+    const productionKey = SafeValue(appConfig?.tracking?.aptabaseProductionKey, ApatabaseKey_Production)
+
+    Aptabase.init(
+        (IsDev() && !__DEV__) ? // mobile (release) dev
+            ApatabaseKey_Dev : // dev
+            productionKey // prodution
+    )
+}
+
+export const TrackingAsync = async ( // main 
+    eventName: string,
+    firebasePaths: string[],
+
+    /**
+     * aptabase, posthog,... (not firebase)
+     */
+    trackingValuesObject?: Record<string, string | number | boolean>
+): Promise<void> => {
+    if (!inited) {
+        console.error('[TrackingAsync] not initted yet.');
+        return
+    }
+
+    const appConfig = await GetRemoteConfigWithCheckFetchAsync()
+
+    if (IsDev())
+        eventName = 'dev__' + eventName
+
+    if (IsLog)
+        console.log('------------------------')
+
+
+    // track aptabase
+
+    const finalAptabaseIgnoredEventNames = await GetFinalAptabaseIgnoredEventNamesAsync()
+
+    const shouldTrackAptabase =
+        (!appConfig || !appConfig.tracking || appConfig.tracking.enableAptabase !== false) &&
+        (!finalAptabaseIgnoredEventNames.includes(eventName))
+
+    console.log(shouldTrackAptabase, inited, finalAptabaseIgnoredEventNames);
+
+    if (shouldTrackAptabase) {
+        AptabaseTrack(eventName, trackingValuesObject)
+
+        if (IsLog) {
+            console.log('tracking [APTABASE]: ', eventName, JSON.stringify(trackingValuesObject));
+        }
+    }
+
+
+    // track firebase
+
+    const shouldTrackFirebase = !appConfig || !appConfig.tracking || appConfig.tracking.enableFirebase !== false
+
+    if (shouldTrackFirebase) {
+        for (let i = 0; i < firebasePaths.length; i++) {
+            let path = GetPrefixFbTrackPath() + firebasePaths[i]
+            path = path.replaceAll('#d', GetTodayStringUnderscore())
+
+            if (IsLog) {
+                console.log('tracking [FIREBASE]: ', path);
+            }
+
+            FirebaseDatabase_IncreaseNumberAsync(path, 0)
+        }
+    }
+
+
+    // track posthog
+
+    const shouldTrackPosthog =
+        !appConfig ||
+        !appConfig.tracking ||
+        appConfig.tracking.enablePosthog !== false
+
+    if (shouldTrackPosthog && posthog) {
+        posthog.capture(eventName, trackingValuesObject)
+
+        if (IsLog) {
+            console.log('tracking [POSTHOG]: ', eventName, JSON.stringify(trackingValuesObject))
+        }
+    }
+
+    if (IsLog)
+        console.log('****************')
+}
+
+export const TrackSimple = (event: string) => { // sub 
+    TrackingAsync(event,
+        [
+            `total/${event}`,
+        ]
+    )
+}
+
+/**
+ * CAN USE ANYWHERE
+ * 
+ * Usage: HandleError(resOrError, 'DataToNotification', false)
+ */
+export const HandleError = (error: any, root: string, alert = true) => {
+    const sError = SafeValue(error?.message, '' + ToCanPrint(error))
+
+    // tracking firebase
+
+    CheckAndTrackErrorOnFirebaseAsync(sError, root)
+
+    // alert
+
+    if (alert) {
+        Alert.alert(
+            'Error',
+            sError
+        )
+    }
+    else if (__DEV__) {
+        const content = `[${root}] ${sError}`
+        console.error(content);
+    }
 }
