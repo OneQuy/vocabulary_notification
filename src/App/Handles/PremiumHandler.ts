7f512a96be30f3509ed8dfbfb30021fa7bc8542a
Minh Quy <onequy@gmail.com>
2024-06-09 21:54:26 +0700

    log

--- src/App/Handles/PremiumHandler.ts
+++ src/App/Handles/PremiumHandler.ts
@@ -17,21 +17,28 @@ import { CanNotSetupUserData, PopupTitleError, RetryText } from "../Hooks/useLoc
 //              - if within 7 days, can change level
 //              - if no, show premium alert
 
+const IsLog = __DEV__
 
 export const CheckSetStartUsingAppTickAsync = async (): Promise<void> => {
     // if did set, then no action
 
     const didSetStartUsingAppTick = await GetBooleanAsync(StorageKey_SetStartUsingAppTick, false)
 
-    if (didSetStartUsingAppTick)
-        return
+    if (didSetStartUsingAppTick) {
+        if (IsLog)
+            console.log('[CheckSetStartUsingAppTickAsync] did set (cached)');
 
+        return
+    }
 
     // need to set, but check if did set on firebase first
 
     const firebaseTickSet = await GetUserValueAsync<number>(UserProperty_StartUsingAppTick)
 
     if (IsNumType(firebaseTickSet)) { // already set
+        if (IsLog)
+            console.log('[CheckSetStartUsingAppTickAsync] did set (cached from firebase)');
+
         SetBooleanAsync(StorageKey_SetStartUsingAppTick, true)
         return
     }
@@ -41,6 +48,9 @@ export const CheckSetStartUsingAppTickAsync = async (): Promise<void> => {
     const nullSuccessOrError = await SetUserValueAsync(UserProperty_StartUsingAppTick, Date.now())
 
     if (nullSuccessOrError === null) { // success
+        if (IsLog)
+            console.log('[CheckSetStartUsingAppTickAsync] set firebase success');
+
         SetBooleanAsync(StorageKey_SetStartUsingAppTick, true)
         return
     }
