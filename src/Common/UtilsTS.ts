4b2b14205a244552a1b171bb2f249e1682f3ce53
Minh Quy <onequy@gmail.com>
2024-06-13 20:01:37 +0700

    IsNot_Null_Undefined_ObjectError

--- src/Common/UtilsTS.ts
+++ src/Common/UtilsTS.ts
@@ -369,6 +369,12 @@ export const IsNumType = (o: any): o is number => {
     return typeof o === 'number' && !Number.isNaN(o)
 }
 
+export const IsNot_Null_Undefined_ObjectError = <T>(anything: any): anything is T => {
+    return anything !== undefined &&
+        anything !== null &&
+        !IsObjectError(anything)
+}
+
 /**
  * 
  * @returns object if true error, in 3 cases:
