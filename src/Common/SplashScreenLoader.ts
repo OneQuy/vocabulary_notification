ba02041ef82be3c95104606c9bccb7876225def3
Minh Quy <onequy@gmail.com>
2024-06-17 22:15:38 +0700

    log

--- src/Common/SplashScreenLoader.ts
+++ src/Common/SplashScreenLoader.ts
@@ -33,7 +33,7 @@ export async function SplashScreenLoader(): Promise<SplashScreenLoaderResult> {
         CheckIsDevAsync(), // (must after GetRemoteConfigWithCheckFetchAsync)
     ])
 
-    // app specific: fetch newly user data
+    // app specific: fetch newly user data (premium,...)
 
     await FetchUserDataOnNewlyInstall.CheckFetchAsync() // ND
 
