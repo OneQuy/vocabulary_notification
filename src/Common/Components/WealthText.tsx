dccef15621174bb9b7bc87908c592faf1a58d697
Minh Quy <onequy@gmail.com>
2024-06-20 08:04:04 +0700

    WealthText

---
+++ src/Common/Components/WealthText.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet, StyleProp, TextStyle } from 'react-native';
+
+export type WealthTextConfig = {
+    text?: string,
+    textStyle?: StyleProp<TextStyle>,
+}
+
+const WealthText = ({
+    textConfigs
+}: {
+    textConfigs?: WealthTextConfig[]
+}) => {
+    if (!textConfigs)
+        return
+
+    return (
+        <View style={styles.container}>
+            {textConfigs.map((config, index) => (
+                <Text
+                    key={index}
+                    style={config.textStyle}
+                >
+                    {config.text}
+                </Text>
+            ))}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    }
+})
+
+export default WealthText
