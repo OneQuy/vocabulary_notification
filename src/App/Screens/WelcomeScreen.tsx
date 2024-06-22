5148cd7d8993d6417fb6b14dc327f6f079637048
Minh Quy <onequy@gmail.com>
2024-06-22 19:12:42 +0700

    outline for welcome screen

--- src/App/Screens/WelcomeScreen.tsx
+++ src/App/Screens/WelcomeScreen.tsx
@@ -7,22 +7,36 @@ import useLocalText from '../Hooks/useLocalText'
 import { CommonStyles } from '../../Common/CommonConstants'
 import WealthText from '../../Common/Components/WealthText'
 import { AppName } from '../../Common/SpecificConstants'
+import { Outline } from '../Constants/Constants_Outline'
+import SlideInView from '../../Common/Components/Effects/SlideInView'
+import { PopuplarityLevelNumber, TotalWords } from '../Constants/AppConstants'
 
 const WelcomeScreen = () => {
     const texts = useLocalText()
 
     const style = useMemo(() => {
         return StyleSheet.create({
-            // master: { flex: 1 },
-            row: { flexDirection: 'row' },
             welcomeTxt: { fontSize: FontSize.Big, color: Color_Text },
-            appNameTxt: { fontSize: FontSize.Big, color: Color_Text, fontWeight: FontBold.BoldMore, },
+
+            contentTxt: { marginVertical: Outline.Normal * 2, textAlign: 'center', fontSize: FontSize.Normal, color: Color_Text },
+
+            contentItemTxt: { marginVertical: Outline.Small, textAlign: 'center', fontSize: FontSize.Normal, color: Color_Text },
+
+            appNameTxt: {
+                fontSize: FontSize.Big,
+                color: Color_Text,
+                fontWeight: FontBold.BoldMore,
+            },
+            
+            itemsView: {
+                alignItems: 'flex-start'
+            },
         })
     }, [])
 
     return (
         <View
-            key={1}
+            key={3}
             style={CommonStyles.flex1_justifyContentCenter_AlignItemsCenter}
         >
             {/* welcome */}
@@ -40,6 +54,30 @@ const WelcomeScreen = () => {
                     ]}
                 />
             </ScaleUpView>
+
+            {/* content */}
+            <ScaleUpView isSpringOrTiming delay={200}>
+                <Text style={style.contentTxt}>{texts.welcome_content.replace('###', AppName)}</Text>
+            </ScaleUpView>
+
+            {/* items */}
+            <View style={style.itemsView}>
+                <SlideInView from={'right'} isSpringOrTiming delay={0}>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={style.contentItemTxt}>⭐️ {texts.welcome_item_0}</Text>
+                </SlideInView>
+
+                <SlideInView from={'right'} isSpringOrTiming delay={100}>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={style.contentItemTxt}>⭐️ {texts.welcome_item_1}</Text>
+                </SlideInView>
+
+                <SlideInView from={'right'} isSpringOrTiming delay={200}>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={style.contentItemTxt}>⭐️ {texts.welcome_item_2.replace('###', PopuplarityLevelNumber.toString())}</Text>
+                </SlideInView>
+
+                <SlideInView from={'right'} isSpringOrTiming delay={300}>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={style.contentItemTxt}>⭐️ {texts.welcome_item_3.replace('###', TotalWords)}</Text>
+                </SlideInView>
+            </View>
         </View>
     )
 }
