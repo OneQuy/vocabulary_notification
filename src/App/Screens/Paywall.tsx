d26786a4531a31e32fdbd1de3344e311a584f20c
Minh Quy <onequy@gmail.com>
2024-06-29 21:23:54 +0700

    paywall

--- src/App/Screens/Paywall.tsx
+++ src/App/Screens/Paywall.tsx
@@ -1,18 +1,25 @@
 // Created on 29 June 2024 (Vocaby)
 
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useCallback, useMemo, useState } from 'react'
+import { View, Text, StyleSheet, StyleProp, TextStyle } from 'react-native'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { FontBold, FontSize } from '../Constants/Constants_FontSize'
 import { Color_BG, Color_Text } from '../Hooks/useTheme'
 import ScaleUpView from '../../Common/Components/Effects/ScaleUpView'
 import useLocalText from '../Hooks/useLocalText'
 import { StartupWindowSize } from '../../Common/CommonConstants'
-import WealthText from '../../Common/Components/WealthText'
-import { AppName } from '../../Common/SpecificConstants'
-import { Outline } from '../Constants/Constants_Outline'
+import WealthText, { WealthTextConfig } from '../../Common/Components/WealthText'
+import { AllIAPProducts, AppName, IapProductMax } from '../../Common/SpecificConstants'
+import { Gap, Outline } from '../Constants/Constants_Outline'
 import { PopuplarityLevelNumber } from '../Constants/AppConstants'
 import LucideIconTextEffectButton from '../../Common/Components/LucideIconTextEffectButton'
 import { BorderRadius } from '../Constants/Constants_BorderRadius'
+import { useMyIAP } from '../../Common/IAP/useMyIAP'
+import { StorageKey_CachedIAP } from '../Constants/StorageKey'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { GetPercentDiscountTxtAndOriginLocalizedPriceTxt, IAPProduct } from '../../Common/IAP/IAP'
+import { SettingItemPanelStyle } from '../Components/SettingItemPanel'
+import { GetRemoteConfigWithCheckFetchAsync } from '../../Common/RemoteConfig'
+import { GetCurrentLifetimeProduct } from '../Handles/AppUtils'
 
 const OffsetEffect = 200
 const DelayStartEffect = 300
@@ -24,7 +31,15 @@ const Paywall = ({
 }) => {
     const texts = useLocalText()
     const [handling, set_handling] = useState(false)
-    const tickStart = Date.now()
+
+    const [currentLifetimeProduct, set_currentLifetimeProduct] = useState<undefined | IAPProduct>(undefined)
+
+    const { isReadyPurchase, localPrice, initErrorObj, fetchedProducts } = useMyIAP(
+        AllIAPProducts,
+        async (s: string) => AsyncStorage.setItem(StorageKey_CachedIAP, s),
+        async () => AsyncStorage.getItem(StorageKey_CachedIAP),
+        currentLifetimeProduct
+    )
 
     const onPressCancelBtn = useCallback(() => {
         // set_handling(true)
@@ -58,6 +73,24 @@ const Paywall = ({
                 fontWeight: FontBold.BoldMore,
             },
 
+            currentPriceTxt: Object.assign(
+                {},
+                SettingItemPanelStyle.explainTxt,
+                {
+                    color: Color_Text,
+                    fontWeight: '800',
+                } as StyleProp<TextStyle>
+            ),
+
+            crossOriginPriceTxt: Object.assign(
+                {},
+                SettingItemPanelStyle.explainTxt,
+                {
+                    textDecorationLine: 'line-through',
+                    color: 'firebrick'
+                } as StyleProp<TextStyle>
+            ),
+
             startBtnTxt: {
                 fontSize: FontSize.Normal
             },
@@ -67,10 +100,74 @@ const Paywall = ({
                 borderRadius: BorderRadius.Small,
                 padding: Outline.Normal,
                 margin: Outline.Normal,
-                marginVertical: Outline.Normal * 2,
+                marginTop: Outline.Normal * 2,
                 maxWidth: StartupWindowSize.width * 0.5,
             },
         })
+    }, [])
+
+    const renderPriceLine = useCallback(() => {
+        const arr: WealthTextConfig[] = [
+            {
+                text: `${localPrice ?? '...'}`,
+                textStyle: style.currentPriceTxt,
+            }
+        ]
+
+        const discountTxts = GetPercentDiscountTxtAndOriginLocalizedPriceTxt(IapProductMax, currentLifetimeProduct, fetchedProducts)
+
+        if (discountTxts) {
+            // percent
+
+            arr.push({
+                text: ` (-${discountTxts.percentDiscountTxt}%, `,
+                textStyle: SettingItemPanelStyle.explainTxt,
+            })
+
+            // origin price
+
+            arr.push({
+                text: discountTxts.originLocalizedPriceTxt,
+                textStyle: style.crossOriginPriceTxt
+            })
+
+            // close bracket
+
+            arr.push({
+                text: ')',
+                textStyle: SettingItemPanelStyle.explainTxt,
+            })
+        }
+
+        return (
+            <WealthText textConfigs={arr} />
+        )
+    }, [localPrice, fetchedProducts, texts, currentLifetimeProduct, SettingItemPanelStyle, style])
+
+    useEffect(() => {
+        (async () => {
+            set_handling(true)
+
+            // fetch premium product id
+
+            const remoteConfig = await GetRemoteConfigWithCheckFetchAsync(false)
+
+            const premiumProduct = GetCurrentLifetimeProduct(remoteConfig)
+
+            set_currentLifetimeProduct(premiumProduct)
+
+            // sale expired line
+
+            // const saleEndTick = SafeValue(remoteConfig?.saleEndTick, 0)
+
+            // if (Date.now() < saleEndTick) {
+            //     set_expirtedSaleLine(`${texts.sale_ends.replace('###', SafeDateString(new Date(saleEndTick), '/'))}`)
+            // }
+
+            // done
+
+            set_handling(false)
+        })()
     }, [])
 
     return (
@@ -122,10 +219,17 @@ const Paywall = ({
 
                     style={style.startBtn}
 
-                    title={texts.start_text}
+                    title={texts.upgrade}
 
                     titleProps={{ style: style.startBtnTxt }}
                 />
+            </ScaleUpView>
+
+            {/* price */}
+            <ScaleUpView delay={OffsetEffect * 3 + DelayStartEffect} isSpringOrTiming>
+                {
+                    renderPriceLine()
+                }
             </ScaleUpView>
         </View>
     )
